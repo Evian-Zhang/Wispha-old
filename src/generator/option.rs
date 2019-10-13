beba4551606c2e022e4ae36914ff000e3762b9c5
Evian-Zhang <evianzhang1999@163.com>
2019-10-13 15:19:56 +0800

    add some implementations to the converting methods

--- src/generator/option.rs
+++ src/generator/option.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
-use std::fmt::{Display, Formatter, Debug, Result};
+use std::fmt;
+use std::fmt::{Display, Formatter, Debug};
 
 use crate::commandline::Generate;
 use crate::config_reader::Config;
@@ -16,8 +17,24 @@ pub enum GenerateLayer {
 }
 
 impl GeneratorOptions {
-    pub fn from_commandline(generate: &Generate) -> Result<GeneratorOptions> {
+    pub fn default() -> GeneratorOptions {
+        GeneratorOptions {
+            layer: GenerateLayer::Flat,
+        }
+    }
 
+    pub fn from_commandline(generate: &Generate) -> Result<GeneratorOptions> {
+        let mut result = GeneratorOptions::default();
+        if generate.flat && generate.recursively {
+            return Err(GeneratorOptionError::FlatAndRecursive);
+        }
+        if generate.recursively {
+            result.layer = GenerateLayer::Recursive;
+        }
+        if generate.flat {
+            result.layer = GenerateLayer::Flat;
+        };
+        Ok(result)
     }
 
     pub fn from_config(config: &Config) -> Result<GeneratorOptions> {
@@ -31,15 +48,18 @@ impl GeneratorOptions {
 
 #[derive(Debug)]
 pub enum GeneratorOptionError {
-
+    FlatAndRecursive,
 }
 
-impl Error for GeneratorOptionError {
-
-}
+impl Error for GeneratorOptionError { }
 
 impl Display for GeneratorOptionError {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "")
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        use GeneratorOptionError::*;
+        match &self {
+            FlatAndRecursive => {
+                write!(f, "Cannot specify flat and recursively at same time.")
+            },
+        }
     }
 }
